64614f20d07dd98eb5ae647d88c5af2c17960283
arafatYasir <arafat.yasir1305@gmail.com>
2026-06-14 23:05:19 +0600

    Added docker file and code to deploy it as monolithic

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,12 @@ import { clerkMiddleware } from '@clerk/express'
 import cors from "cors";
 import { NODE_ENV, PORT, SITE_URL } from "./config/env.js";
 import { connectToDatabase } from "./lib/db.js";
+import fs from "fs";
+import path from "path";
 
 const app = express();
+
+const publicDir = path.join(process.cwd(), "public");
 
 // Middlewares
 app.use(express.json());
@@ -13,7 +17,17 @@ app.use(clerkMiddleware());
 
 app.get("/health", (req, res) => {
     res.status(200).json({ success: true });
-})
+});
+
+// if the public directory exists, serve the static files
+// this is for the production build
+if (fs.existsSync(publicDir)) {
+    app.use(express.static(publicDir));
+
+    app.get("/{*any}", (req, res, next) => {
+        res.sendFile(path.join(publicDir, "index.html"), (e) => next(e));
+    });
+}
 
 app.listen(PORT, async () => {
     console.log(`Server is running on port: ${PORT} in ${NODE_ENV} mode`);
