31e101320bf2c40bcc2b9221839312796a497c27
arafatYasir <arafat.yasir1305@gmail.com>
2026-06-15 22:18:13 +0600

    Initialize frontend project with React, React Router, Clerk authentication, and initial Auth/Chat pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,25 @@
-import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
-import { Button } from '@heroui/react';
 import { ThemeProvider } from "./context/ThemeContext";
-import { WallpaperProvider } from "./context/WallpaperContext";
+import { WallpaperProvider } from "./context/WallpaperContext"
+import { Navigate, Route, Routes } from 'react-router';
+import ChatPage from './pages/ChatPage';
+import AuthPage from './pages/AuthPage';
+import { useAuth } from "@clerk/react";
 
 function App() {
+  const { isSignedIn, isLoaded } = useAuth();
+
+  // I will make this a better component
+  if (!isLoaded) {
+    return <div>Loading....</div>
+  }
 
   return (
     <ThemeProvider>
       <WallpaperProvider>
-        <div className='flex justify-center'>
-          <Button>My Lovely Button</Button>
-          <header>
-            <Show when="signed-out">
-              <SignInButton mode="modal" />
-              <SignUpButton mode="modal" />
-            </Show>
-            <Show when="signed-in">
-              <UserButton />
-            </Show>
-          </header>
-        </div>
+        <Routes>
+          <Route path="/" element={isSignedIn ? <ChatPage /> : <Navigate to="/auth" replace />} />
+          <Route path="/auth" element={!isSignedIn ? <AuthPage /> : <Navigate to="/" replace />} />
+        </Routes>
       </WallpaperProvider>
     </ThemeProvider>
   )
