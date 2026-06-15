a2b932cf31727b609d793496b9cec6dd14aac6ef
arafatYasir <arafat.yasir1305@gmail.com>
2026-06-15 20:40:58 +0600

    Implemented theme and wallpaper management contexts with preset support

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,27 @@
-import './App.css'
 import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
+import { Button } from '@heroui/react';
+import { ThemeProvider } from "./context/ThemeContext";
+import { WallpaperProvider } from "./context/WallpaperContext";
 
 function App() {
 
   return (
-    <div>
-      <header>
-        <Show when="signed-out">
-          <SignInButton mode="modal" />
-          <SignUpButton mode="modal" />
-        </Show>
-        <Show when="signed-in">
-          <UserButton />
-        </Show>
-      </header>
-    </div>
+    <ThemeProvider>
+      <WallpaperProvider>
+        <div className='flex justify-center'>
+          <Button>My Lovely Button</Button>
+          <header>
+            <Show when="signed-out">
+              <SignInButton mode="modal" />
+              <SignUpButton mode="modal" />
+            </Show>
+            <Show when="signed-in">
+              <UserButton />
+            </Show>
+          </header>
+        </div>
+      </WallpaperProvider>
+    </ThemeProvider>
   )
 }
 
